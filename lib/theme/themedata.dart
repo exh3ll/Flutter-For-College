0cef13644bd67c58f35918d5eeda36e647ebdb17
blissexodia <binnoley@gmail.com>
2025-12-12 11:58:29 +0545

    themedata

--- lib/theme/themedata.dart
+++ lib/theme/themedata.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_for_college/screens/dashboard_screen.dart';
 
+ThemeData getAppThemeData() {
+  return ThemeData(
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    useMaterial3: true,
+    fontFamily: 'OpenSans regular',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      ),
+    ),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Colors.deepPurple,
+      foregroundColor: Colors.white,
+      centerTitle: true,
+      elevation: 4,
+    ),
+  );
+}
